5bd6a59b7ec44816253f1972d0c65f8b1f7e3837
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-26 09:45:49 +0530

    Matrixe multiplication

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -9,12 +9,12 @@ class MatrixMultiplication {
 		int a[][] = { { 1, 5 }, { 2, 7 }, { 8, 3 } };
 		int b[][] = { { 4, 1 }, { 9, 2 }, { 3, 2 } };
 
-		int c[][] = new int[2][2];
+		int c[][] = new int[3][3];
 
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				c[i][j] = 0;
-				for (int k = 0; k < 2; k++) {
+				for (int k = 0; k < 3; k++) {
 					c[i][j] += a[i][k] * b[k][j];
 				}
 				System.out.print(c[i][j] + " ");
