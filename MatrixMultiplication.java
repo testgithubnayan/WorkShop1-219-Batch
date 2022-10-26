a3cb09d7dba702adbed1288810b639091bb3c733
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-10-26 09:37:05 +0530

    Matrixe Multiplication Having 2 * 2 power

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -3,7 +3,7 @@ package com.brigelabz;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class MatrixMultiplicationExample {
+class MatrixMultiplication {
 	public static void main(String args[]) {
 
 		int a[][] = { { 1, 5 }, { 2, 7 }, { 8, 3 } };
